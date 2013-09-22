92afbb4637213220e357bf3bce0dbacc7c405c3e
vosecek <jiri.vosecek@gmail.com>
2013-09-22 14:34:11 +0200

    Podpora pro kontrolu aktuálnosti storu Matches v Modelu.

--- app/model/Session.js
+++ app/model/Session.js
@@ -7,7 +7,8 @@ Ext.define('catcher.model.Session', {
 	      {name: 'tournament_id', type: 'int'},
 	      {name: 'tournament_name', type: 'string'},
 	      {name: 'timestamp_logged', type: 'int'},
-	      {name: 'active_team_node', type: 'string'}
+	      {name: 'active_team_node', type: 'string'},
+        {name: 'match_reload', type: 'date', dateFormat: "timestamp", defaultValue: 1},
 	  ]
 	}
 });
